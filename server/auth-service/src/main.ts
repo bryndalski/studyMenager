6090b42bfa807da46eeedd4781427942247d305b
bryndalski <kubabryndal@gmail.com>
2022-11-18 10:31:24 +0100

    add docker rabbitmq communication

--- server/auth-service/src/main.ts
+++ server/auth-service/src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://admin:admin@kroliczek:5672'],
+        queue: 'auth',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  )
+  console.log('====================================')
+  console.log('auth słucha')
+  console.log('====================================')
+  await app.listen()
 }
-bootstrap();
+bootstrap()
--- server/main-server/src/main.ts
+++ server/main-server/src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { Logger } from '@nestjs/common'
+
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://localhost:5672'],
-        queue: 'auth_queue',
-        queueOptions: {
-          durable: false,
-        },
-      },
-    },
-  )
-  await app.listen()
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
 bootstrap()
